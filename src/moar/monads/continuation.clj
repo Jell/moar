b61bb58d962ad45363362639bacf08fdfaadb918
Jell <jean-louis@jawaninja.com>
2014-07-17 12:22:02 +0200

    Refactor continuation monad in preparation for continuation transformer

--- src/moar/monads/continuation.clj
+++ src/moar/monads/continuation.clj
@@ -1,53 +1,38 @@
 (ns moar.monads.continuation
   (:require [moar.protocols :refer :all]))
 
-(declare continue-with make-continuation)
+(deftype ContinuationFn [m-impl fun]
+  MonadInstance
+  (monad-implementation [_] m-impl)
+  clojure.lang.IFn
+  (invoke [_ callback] (fun callback)))
 
-(defprotocol Continuation
-  (call-continuation [continuation callback]
-    "invoke the continuation with a given callback"))
+(defn continuation-fn [m-impl fun]
+  (ContinuationFn. m-impl fun))
+
+(defprotocol MonadContinuation
+  (callcc [self fun]))
 
 (defrecord ContinuationMonad []
+  MonadContinuation
+  (callcc [self fun]
+    (continuation-fn self
+                     (fn [callback]
+                       ((fun (fn [value]
+                               (continuation-fn
+                                self
+                                (fn [_callback]
+                                  (callback value)))))
+                        callback))))
   Monad
-  (wrap* [_ val] (continue-with val))
-  (bind* [_ m-val m-fun]
-    (make-continuation
+  (wrap* [self val]
+    (continuation-fn self (fn [callback] (callback val))))
+  (bind* [self m-val m-fun]
+    (continuation-fn
+     self
      (fn [callback]
-       (call-continuation
-        m-val
+       (m-val
         (fn [val]
-          (call-continuation (m-fun val) callback)))))))
+          ((m-fun val) callback)))))))
 
 (def monad (ContinuationMonad.))
-
-(deftype ContinuationFunction [function]
-  clojure.lang.IFn
-  (invoke [_ callback] (function callback))
-  Continuation
-  (call-continuation [_ callback] (function callback))
-  MonadInstance
-  (monad-implementation [_] monad))
-
-(defn make-continuation
-  "construct a continuation function"
-  [function]
-  (ContinuationFunction. function))
-
-(defn continue-with
-  "create a continuation function that calls
-   the continuation with the given value"
-  [value]
-  (make-continuation (fn [callback] (callback value))))
-
-(defn callcc
-  "takes a function and calls it with the current continuation
-   and returns a continuation monad"
-  [fun]
-  (make-continuation
-   (fn [callback]
-     (call-continuation
-      (fun (fn [value]
-             (make-continuation
-              (fn [_callback]
-                (callback value)))))
-      callback))))
--- test/moar/monads/continuation_test.clj
+++ test/moar/monads/continuation_test.clj
@@ -1,22 +1,24 @@
 (ns moar.monads.continuation-test
-  (:use [clojure.test]
-        [moar.monads.continuation])
-  (:require [moar.core :as m]))
+  (:require [clojure.test :refer :all]
+            [moar.monads.continuation :as continuation]
+            [moar.core :refer :all]))
 
 (deftest continuation-tests
+  (let [monad continuation/monad
+        return (partial wrap monad)
+        callcc (partial continuation/callcc monad)]
 
-  (testing "can compose continuations"
-    (let [c (m/mlet
-             [a (continue-with 1)
-              b (continue-with 2)]
-             (continue-with (+ a b)))]
-      (is (= 3 (call-continuation c identity)))))
+    (testing "can compose continuations"
+      (let [c (mlet
+               [a (return 1)
+                b (return 2)]
+               (return (+ a b)))]
+        (is (= 3 (c identity)))))
 
-  (testing "can use callcc"
-    (let [result
-          (call-continuation
-           (m/mlet
-            [x (callcc (fn [cont] (continue-with cont)))]
-            (x (fn [_] (continue-with ::val))))
-           identity)]
-      (is (= result ::val)))))
+    (testing "can use callcc"
+      (let [result
+            ((mlet
+              [x (callcc (fn [cont] (return cont)))]
+              (x (fn [_] (return ::val))))
+             identity)]
+        (is (= result ::val))))))
