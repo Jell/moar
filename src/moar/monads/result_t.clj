47cf2b9d226c925c619e53401e94672c006ae3e7
Jell <jean-louis@jawaninja.com>
2014-07-17 10:28:07 +0200

    Cleanup

--- src/moar/monads/result_t.clj
+++
@@ -1,8 +0,0 @@
-(ns moar.monads.result-t
-  (:require [moar.protocols :refer :all]
-            [moar.core :refer :all]
-            [moar.monads.result :as result])
-  (:import [moar.monads.result ResultMonad]))
-
-(extend ResultMonad
-  )
